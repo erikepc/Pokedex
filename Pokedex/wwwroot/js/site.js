82790def84414a41f937c555c81b410c72d85fb6
erikepc <erike45@hotmail.com>
2024-09-20 21:54:45 -0300

    Filtro em JS finalizado

--- Pokedex/wwwroot/js/site.js
+++ Pokedex/wwwroot/js/site.js
@@ -1,4 +1,26 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function filter(tipo) {
+    // Alterar a visibilidade dos cards
+    let cardCount = 0;
+    document.querySelectorAll('.poke').forEach(card => {
+        card.style.display = "flex";
+        cardCount += 1;
+        if (!card.classList.contains(tipo) && tipo !== ''){
+            card.style.display = 'none';
+            cardCount -= 1; 
+        }    
+    });
 
-// Write your JavaScript code.
+    // Verificar se existem cards com operador ternario
+    let zeroPokemon = document.querySelector('#zeroPokemon');
+    cardCount == 0 ? zeroPokemon.classList.remove('d-none') : zeroPokemon.classList.add('d-none');
+
+    //Alterar o estilo dos botões de filtro
+    document.querySelectorAll('.btn-filter').forEach(button => {
+        button.classList.add('btn-sm');
+        button.classList.remove('btn-md');
+        if (button.id == `btn-${tipo}`) {
+            button.classList.remove('btn-sm');
+            button.classList.add('btn-md');
+        }
+    });
+}
